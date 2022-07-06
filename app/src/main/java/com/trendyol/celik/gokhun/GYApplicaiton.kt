e9c53819555c60a6a422e5876271ee31b382b7ac
gokhun.celik <celik.gokhun@gmail.com>
2022-07-06 17:33:27 +0300

    - night mode set as default theme.

--- app/src/main/java/com/trendyol/celik/gokhun/GYApplicaiton.kt
+++ app/src/main/java/com/trendyol/celik/gokhun/GYApplicaiton.kt
@@ -1,9 +1,16 @@
 package com.trendyol.celik.gokhun
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GYApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
 
 }
