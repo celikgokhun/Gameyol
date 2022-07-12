2e30dc64caf9bb5440d26e83713be0cb4a549e1b
gokhun.celik <celik.gokhun@gmail.com>
2022-07-12 12:58:47 +0300

    - mockK lib. implemented. - gameListingUseCaseTest created.

---
+++ app/src/test/java/com/trendyol/celik/gokhun/gamelisting/GameListingUseCaseTest.kt
@@ -0,0 +1,78 @@
+package com.trendyol.celik.gokhun.gamelisting
+
+import com.trendyol.celik.gokhun.base.extensions.ResourceReactiveExtensions.remote
+import com.trendyol.celik.gokhun.data.gamelisting.repository.GameListingRepository
+import com.trendyol.celik.gokhun.data.gamelisting.source.remote.model.response.list.GameListingResponse
+import com.trendyol.celik.gokhun.domain.gamelisting.GameListingMapper
+import com.trendyol.celik.gokhun.domain.gamelisting.GameListingUseCase
+import io.reactivex.rxjava3.core.Observable
+import org.junit.Before
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import org.junit.Test
+
+class GameListingUseCaseTest {
+    @MockK
+    lateinit var gameListingRepository: GameListingRepository
+
+    @MockK
+    lateinit var gameListingMapper: GameListingMapper
+
+    lateinit var gameListingUseCase: GameListingUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        gameListingUseCase = GameListingUseCase(
+            gameListingRepository,
+            gameListingMapper
+        )
+    }
+
+    @Test
+    fun `given successful response, when fetchGames called, then should return game listing`() {
+        // Given
+        val response = GameListingResponse(
+            null,null,null,
+            null,null,null,
+            null,null,null,
+            null,null,null,
+            null
+        )
+
+        every {
+            gameListingRepository.fetchGames()
+        } answers { Observable.just(response).remote() }
+
+        // When
+        gameListingUseCase.fetchGames()
+
+        // Then
+        verify { gameListingRepository.fetchGames() }
+    }
+
+    @Test
+    fun `given successful response, when fetchNextGames called, then should return game listing`() {
+        // Given
+        val next :String? = "2"
+        val response = GameListingResponse(
+            null,null,null,
+            null,null,null,
+            null,null,null,
+            null,null,null,
+            null
+        )
+
+        every {
+            gameListingRepository.fetchNextGames(next)
+        } answers { Observable.just(response).remote() }
+
+        // When
+        gameListingUseCase.fetchNextGames(next)
+
+        // Then
+        verify { gameListingRepository.fetchNextGames(next) }
+    }
+}
